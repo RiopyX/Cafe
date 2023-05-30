b183c665f2ee4a8f32757033dcfcab4c5be13a7e
Lyapese <Kamilla.bu4@gmail.com>
2023-05-31 00:56:10 +0300

    erger

--- app/src/main/java/com/example/cafe2/Cafe.java
+++ app/src/main/java/com/example/cafe2/Cafe.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Gravity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -17,6 +20,9 @@ public class Cafe extends AppCompatActivity {
 
     ImageView imgGuest, imgCafe;
     Button btnMenu, btnScote;
+    TextView moneyView;
+    int money = 0;
+    int toast;
     private final Handler handler = new Handler();
     private final int GUEST_ANIMATION_DURATION = 4000;
     private final int GUEST_LEAVE_DELAY = 2000;
@@ -43,6 +49,7 @@ public class Cafe extends AppCompatActivity {
         imgGuest = findViewById(R.id.guest);
         btnMenu = findViewById(R.id.btnMenu);
         btnScote = findViewById(R.id.btnScote);
+        moneyView = findViewById(R.id.moneyView);
         Animation cafeStart = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.cafe_start);
         Animation buttonAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.button_anim);
         btnMenu.startAnimation(buttonAnim);
@@ -68,7 +75,7 @@ public class Cafe extends AppCompatActivity {
     public void setImage() {
             Random random2 = new Random();
             int number = random2.nextInt();
-            int[] selectedArray = (number <= 10) ? guests:superguests;
+            int[] selectedArray = (number <= 10) ? superguests:guests;
             int randomIndex = random2.nextInt(selectedArray.length);
             int randomImage = selectedArray[randomIndex];
             imgGuest.setImageResource(randomImage);
@@ -92,6 +99,25 @@ public class Cafe extends AppCompatActivity {
 
                             @Override
                             public void onAnimationEnd(Animation animation) {
+                                if (number <= 10){
+                                    toast = 7;
+                                    money = money + 7;
+                                } else {
+                                    toast = 3;
+                                    money = money +3;
+                                }
+                                String message = String.valueOf(toast);
+                                String Money = String.valueOf(money);
+                                Toast toast = new Toast(getApplicationContext());
+                                View toastView = getLayoutInflater().inflate(R.layout.custom_toast, null);
+                                toast.setView(toastView);
+                                toast.setDuration(Toast.LENGTH_SHORT);
+                                toastView.setBackgroundResource(R.drawable.transparent_toast_background);
+                                TextView toastText = toastView.findViewById(R.id.toastView);
+                                toastText.setText("+ " + message);
+                                toast.setGravity(Gravity.CENTER, 0, -30);
+                                toast.show();
+                                moneyView.setText(Money);
                                 setImage();
                             }
 
